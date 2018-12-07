e35a5d5a8456ddbba32c47fee051ee43c5e489d0
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-12-07 18:26:20 +0900

    change_defaultToLikeButton

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
+    <LikeButton/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import LikeButton from './components/LikeButton'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LikeButton
   }
 }
 </script>
--- src/components/LikeButton.vue
+++ src/components/LikeButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="LikeButton">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -85,7 +85,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'LikeButton',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
